dd590fee5d5c294123c0641745ba3f8ed9c93c9c
amryami <amr.yami1@gmail.com>
2023-11-09 00:08:59 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const redis = require('redis');
-const { Client } = require('pg');
 
 const PORT = 4000;
 const app = express();
@@ -14,31 +13,11 @@ const redisClient = redis.createClient({
     url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
 });
 
-//connect to postgres
-
-//connect db
-const DB_USER_POSTGRES = "root";
-const DB_PASSWORD_POSTGRES = "example";
-const DB_PORT_POSTGRES = 5432;
-const DB_HOST_POSTGRES = 'postgres'
-
-const URI_POSTGRES = `postgresql://${DB_USER_POSTGRES}:${DB_PASSWORD_POSTGRES}@${DB_HOST_POSTGRES}:${DB_PORT_POSTGRES}`;
-
-const client = new Client({
-    connectionString: URI_POSTGRES,
-  });
-
-  client.connect()
-    .then(() => console.log('connected to postgres'))
-    .catch((err) => console.log('failed postgres', err));
-
-    //redis
-
 redisClient.on('error', err => console.log('Redis Client Error', err));
 redisClient.on('connect', err => console.log('connected to redis'));
 redisClient.connect();
 
-//connect mongo db
+//connect db
 const DB_USER = "root";
 const DB_PASSWORD = "example";
 const DB_PORT = 27017;
@@ -54,12 +33,12 @@ mongoose
     .catch((err) => console.log('failed', err));
 
     app.get('/', (req, res) => {
-        redisClient.set('key', 'value');
+        redisClient.set('key', 'test docker hub');
          res.send('<h1>hello amro yami dev</h1>');
     });
     app.get('/data', async (req, res) => {
        const val = await redisClient.get('key');
-         res.send(`<h1>welcome amro yami dev</h1><h2>${val}</h2>`);
+         res.send(`<h1>hello amro yami dev</h1><h2>${val}</h2>`);
     });
 
 app.listen(PORT, () => console.log(`app is up and running on port : ${PORT}`))
